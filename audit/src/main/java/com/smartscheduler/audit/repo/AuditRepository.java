0acdfff004c87edc1121f49933a409a5fa7021ae
Stalin Jeyaraj <mariasstalin@gmail.com>
2025-10-31 09:30:51 +0530

    Docker issues fixed

--- audit/src/main/java/com/smartscheduler/audit/repo/AuditRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.smartscheduler.audit.repo;
-
-import com.smartscheduler.audit.model.AuditLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditRepository extends JpaRepository<AuditLog, Long> {
-}
--- rescheduler/run-local.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Rescheduler
-cd /d "%~dp0"
-
-mvn spring-boot:run -Dspring-boot.run.profiles=local -DskipTests
-
-echo.
-echo Press any key to exit...
-pause > nul
